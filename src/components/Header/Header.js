82e5980f1f8682946a6be59300a0b45ae0308725
shahil kv <mshahilkv@gmail.com>
2022-06-28 13:37:23 +0530

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import React from 'react';
+// this is importing the icons from the react icons pack
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { DiCssdeck } from 'react-icons/di';
+
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+
+const Header = () =>  (
+  <div>
+    Header
+  </div>
+);
+
+export default Header;
