58b70ebfd2d801a68c6f71f8e9e5f7c442f84a9e
shahil kv <mshahilkv@gmail.com>
2022-06-29 00:36:07 +0530

    add basepath and assetprefix

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */  
+
+  const nextConfig = {
+    reactStrictMode:true,
+    images:{
+        loader:"akamai",
+        path:"",
+    },
+    basePath:"/nextjs-pages",
+    assetPrefix:"/nextjs-pages"
+  };
+  module.exports= nextConfig;
