e8fa72ded1ff94d8c017ee995101b5edf7bf1cf3
shahil kv <mshahilkv@gmail.com>
2022-06-28 18:28:26 +0530

    Added the accomplishment website

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+// we have imported several custom elements from other files such as global 
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import { Box, Boxes, BoxNum, BoxText, } from './AcomplishmentsStyles';
 
+// this is the data that we want to show to the section 
 const data = [
   { number: 20, text: 'Open Source Projects'},
   { number: 1000, text: 'Students', },
@@ -10,9 +12,24 @@ const data = [
   { number: 5000, text: 'Github Stars', }
 ];
 
+// we are starting the section 
 const Acomplishments = () => (
   <div>
-    Acomplishments
+   <Section>
+   {/* this is the title for the personal accomplisment large title */}
+    <SectionTitle> Personal Accomplishment </SectionTitle>
+    <Boxes>
+  //  we are using a map function to give Each and every function to get elements
+// we are using the card as the data and the index as the number of datas in the
+         {data.map((card,index)=>(
+          <Box key={index}>
+          we include our Number of achievements in the boxnum as card.num
+          <BoxNum>${card.number}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+          </Box>
+         ) )} 
+    </Boxes>
+   </Section>
   </div>
 );
 
